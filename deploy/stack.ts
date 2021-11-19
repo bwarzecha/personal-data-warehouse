2cf0fdc4cd575493afed2f24e4c529a42df12cdd
Bartosz Warzecha <bartosz@warzecha.co.uk>
2021-11-19 13:53:19 +0000

    Changing node setup to v2

--- deploy/stack.ts
+++ deploy/stack.ts
@@ -4,8 +4,13 @@ import * as cdk from "@aws-cdk/core";
 export class NextStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
-    new NextJSLambdaEdge(this, "PersonalDataWarehouse", {
+    const lambda = new NextJSLambdaEdge(this, "PersonalDataWarehouse", {
       serverlessBuildOutDir: "./build",
+    });
+
+    new cdk.CfnOutput(this, "website", {
+      value: `https://${lambda.distribution.domainName}`,
+      description: "Domain Name of the stack",
     });
   }
 }
